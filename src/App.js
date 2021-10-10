d7ed1b62f118f6678a219f9bc6a353bf7b5f2036
Ivan Faryga <ifaryga@gmail.com>
2021-10-10 21:33:15 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ import ContactList from "./Components/ContactList/ContactList";
 import "./App.css";
 import Filter from "./Filter/Filter";
 
+const initialState = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
+
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
+
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem("contacts"));
+    // const contacts = localStorage.getItem("contacts");
+    // const parsedContacts = JSON.parse(contacts);
+    // this.setState({ contacts: parsedContacts });
+
+    // if (parsedContacts !== null) {
+    //   this.setState({ contacts: parsedContacts });
+    // }
+
+    this.setState({ contacts: parsedContacts ?? initialState }); // ??  --- https://learn.javascript.ru/nullish-coalescing-operator
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   formSubmitHandler = (object) => {
     const { name, number } = object;
